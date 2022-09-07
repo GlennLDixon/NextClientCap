4a2052440ee9f7afe9303818fb0be10063f9b912
Glenn Dixon <glenn.dixon@groups360.com>
2022-09-07 11:58:51 -0500

    Added login functionality, imported Navbar and ApplicationViews components into the next.js component, Application views still needs work

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,4 @@
+import React from "react"
+import { Route, Routes, Navigate } from "react-router-dom"
+import { Login } from './auth/Login'
+import { Reqister } from './auth/Register'
---
+++ src/components/Next.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react"
+import { ApplicationViews } from "./ApplicationViews"
+import { NavBar } from "./nav/NavBar"
+import "./Next.css"
+
+export const Next = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(sessionStorage.getItem("next_user") !== null)
+
+    const setAuthUser = (user) => {
+        sessionStorage.setItem("next_user", JSON.stringify(user))
+        setIsAuthenticated(sessionStorage.getItem("next_user") !== null)
+    }
+
+    const clearUser = () => {
+        sessionStorage.clear()
+        setIsAuthenticated(sessionStorage.getItem("next_user") !== null)
+    }
+    return (
+        <>
+            <NavBar clearUser={clearUser} isAuthenticated={isAuthenticated} />
+            <ApplicationViews setAuthUse={setAuthUser} isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
+        </>
+    )
+
+}
