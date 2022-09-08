fbb2108aa8de2ac3e70333c2d9f7db4fe0e9d071
Glenn Dixon <glenn.dixon@groups360.com>
2022-09-08 16:09:48 -0500

    created a basic html layout for my content that will be going on our page

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,4 +1,28 @@
 import React from "react"
-import { Route, Routes, Navigate } from "react-router-dom"
+import { Route, useRoutes, Navigate, Link } from "react-router-dom"
 import { Login } from './auth/Login'
-import { Reqister } from './auth/Register'
+import { Register } from './auth/Register'
+import "./ApplicationViews.css"
+
+export const ApplicationViews = ({ isAuthenticated, setIsAuthenticated }) => {
+    const PrivateRoute = ({ children }) => {
+        return isAuthenticated ? children : <Link to="/login" />
+    }
+
+    const setAuthUser = (user) => {
+        sessionStorage.setItem("next_user", JSON.stringify(user))
+        setIsAuthenticated(sessionStorage.getItem("next_user") !== null)
+    }
+
+    return (
+        <div className="landingPageContainer">
+            <div className="landingLeftContent">
+                <h1>Hello World</h1>
+            </div>
+            <div className="landingRightContent">
+                <h1>Hello World</h1>
+            </div>
+
+        </div>
+    )
+}
