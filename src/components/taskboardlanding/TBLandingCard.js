63919c49bb88c0619eb74b03494d13622e0d9dc0
Glenn Dixon <glenn.dixon@groups360.com>
2022-09-11 19:16:43 -0500

    created the card for the boards

---
+++ src/components/taskboardlanding/TBLandingCard.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const TBLandingCard = ({ }) => {
+    return (
+        <div className="card">
+            <div className="card-content">
+                <h4>Hello</h4>
+                <button type="button" onClick={() => handleDeleteCard(card.id)}>X</button>
+            </div>
+        </div>
+    )
+}
