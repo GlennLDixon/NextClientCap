4ecebd0e99ca7c1d21a5f349b2f8e153f59696e6
Glenn Dixon <glenn.dixon@groups360.com>
2022-09-15 19:45:25 -0500

    imported the props for the card to display the board name

--- src/components/taskboardlanding/TBLandingCard.js
+++ src/components/taskboardlanding/TBLandingCard.js
@@ -1,11 +1,11 @@
 import React from "react"
 
-export const TBLandingCard = ({ }) => {
+export const TBLandingCard = ({ handleDeleteBoard, board }) => {
     return (
         <div className="card">
             <div className="card-content">
-                <h4>Hello</h4>
-                <button type="button" onClick={() => handleDeleteCard(card.id)}>X</button>
+                <h4>{board.name}</h4>
+                <button type="button" onClick={() => handleDeleteBoard(board.id)}>X</button>
             </div>
         </div>
     )
