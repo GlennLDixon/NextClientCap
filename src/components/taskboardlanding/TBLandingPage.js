35bfbb8f1f038891955234176b847c518ad9fcb5
Glenn Dixon <glenn.dixon@groups360.com>
2022-09-15 19:42:58 -0500

    imported TBlandingList component

--- src/components/taskboardlanding/TBLandingPage.js
+++ src/components/taskboardlanding/TBLandingPage.js
@@ -1,5 +1,6 @@
+import { TBLandingList } from "./TBLandingList"
+
 export const TaskBoardLandingPage = () => {
-    const [taskBoardCards, setTaskBoardCards] = ([])
 
     return (
         <div className="taskBoardPageContainer">
@@ -7,7 +8,8 @@ export const TaskBoardLandingPage = () => {
                 <button className="NewBoardBtn">Create New Board</button>
             </div>
             <div className="TaskBoardsList">
-                <h1>Task Board List Goes here</h1>
+                <h1>Board List</h1>
+                <TBLandingList />
             </div>
         </div>
     )
