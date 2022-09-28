dae8557b5dbb46c97153b130d4ae5a1ad6c59ae2
Glenn Dixon <glenn.dixon@groups360.com>
2022-09-28 16:22:33 -0500

    change link path from taskboardpage to tasklistpage

--- src/components/taskboardlanding/TBLandingCard.js
+++ src/components/taskboardlanding/TBLandingCard.js
@@ -5,7 +5,7 @@ export const TBLandingCard = ({ handleDeleteBoard, board }) => {
     return (
         <div className="card">
             <div className="card-content">
-                <Link to={`/taskboardpage/${board.id}`}>
+                <Link to={`/tasklistpage/${board.id}`}>
                     <h4>{board.name}</h4>
                 </Link >
                 <button type="button" onClick={() => handleDeleteBoard(board.id)}>X</button>
